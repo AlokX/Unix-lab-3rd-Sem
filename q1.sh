b5350a2a2dce4bf68c96c69bb8c6a40dd5086ce8
Alok <aloksarkarxxxx@gmail.com>
2021-02-06 23:03:13 +0530

    Q[1] done

--- q1.sh
+++ q1.sh
@@ -3,3 +3,21 @@
 # Write a program to print all the prime numbers from 1 to 300.
 # Use nested loops, break & continue.
 # <----------QUESTION----------->
+
+#!/bin/bash
+for i in {2..300} # for each number 
+do
+    flag=0
+    for (( j=2; j<= $(( i/2 )); j++ ))
+    do
+        if [ $((i%j)) == 0 ]; then
+            flag=1
+            break
+        fi
+    done
+    if [ $flag -eq 1 ]; then
+        continue
+    fi
+    echo -n "$i "
+done
+echo
