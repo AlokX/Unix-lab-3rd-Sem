3f15bb4ca57b6108c76d426c6e8b56dd5eeec442
Alok <aloksarkarxxxx@gmail.com>
2021-02-09 21:15:44 +0530

    [Q1] done

--- q2.sh
+++ q2.sh
@@ -1,3 +1,17 @@
 # <----------QUESTION----------->
 # Write a program to generate all combinations of 1,2 and 3 using for loops.
 # <----------QUESTION----------->
+
+#!/bin/bash
+for i in 1 2 3 
+do
+    for j in 1 2 3 
+    do
+        for k in 1 2 3 
+        do
+            echo -n "$i$j$k "
+        done
+    done
+done
+echo
+
